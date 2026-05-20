1ba9e9a31b6624c41f7b9457d85ca198d1c14e97
Vilmir Yakupov <pirogolik@yandex.ru>
2026-05-20 16:30:39 +0300

    exam autotests v.2

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -99,7 +99,7 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 			"Длительность: %.2f ч.\n"+
 			"Дистанция: %.2f км.\n"+
 			"Скорость: %.2f км/ч\n"+
-			"Сожгли калорий: %.2f",
+			"Сожгли калорий: %.2f\n",
 			duration.Hours(), distanceKm, speed, calories), nil
 
 	default:
